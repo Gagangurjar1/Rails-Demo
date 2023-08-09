382a320307f08b9809b5953422cce9cd40b8a4d4
gagangurjar <gagansihore@gmail.com>
2023-08-09 19:15:52 +0530

    update

---
+++ app/models/gurjar.rb
@@ -0,0 +1,2 @@
+class Gurjar < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_073655) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_112530) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_073655) do
   end
 
   create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "gurjars", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
